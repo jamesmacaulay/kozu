6531ceb199e00c534d4dc32487bd65c67c4fbff5
James MacAulay <james@shopify.com>
2014-01-05 01:53:04 -0500

    WIP some ideas

--- app/js/kozu.js
+++ app/js/kozu.js
@@ -5,99 +5,37 @@
     return typeof x === 'function';
   }
 
+  // "has a .then method"
   function isThenable(x) {
     return x != null && isFunction(x.then);
   }
 
+  // build a promise
   function promise(resolver) {
     return new Promise(resolver);
   }
 
-  // // returns f(g(args...)), preserving `this`
-  // function compose2LR(g, f) {
-  //   return (function() {
-  //     return f.call(this, g.apply(this, arguments));
-  //   });
-  // }
-
-  // function compose() {
-  //   return reduce(compose2LR, arguments);
-  // }
-
-  function identity(x) { return x; }
-
-  function any(items, func) {
-    var i = items.length;
-    while (i--) { if (func(items[i])) { return true; } }
-    return false;
-  }
-
   function buildKozu(deps) {
+    // default isPromise
     var isPromise = (deps && deps.isPromise) || isThenable;
 
-    function whenever(value, func) {
-      if (isPromise(value) || isPromise(func)) {
-        return Promise.all([value, func]).then(function(args) {
-          return args[0].then(args[1]);
-        });
-      } else {
-        return func(value);
-      }
-    }
-
-    function applyAgnostic(func, ctx, args) {
-      var i;
-      if (isPromise(func) || isPromise(ctx) || any(args, isPromise)) {
-        return Promise.all([func, ctx, Promise.all(slice.call(args))]);
-      } else {
-        return func.apply(ctx, args);
-      }
-    }
-
-    function agnostic(func) {
-      return (function() {
-        return applyAgnostic(func, this, arguments);
-      });
-    }
-
-    function deepAgnostic(func) {
-      if (isFunction(func)) {
-        return (function() {
-          return applyAgnostic(func, this, _.map(arguments, deepAgnostic));
-        });
-      } else {
-        return func;
-      }
-    }
-
-    var map = deepAgnostic(_.map);
-
-    // function map(items, func) {
-    //   return whenever(func, function(func) {
-    //     var i, len, results, item, promise, hasPromises;
-    //     len = items.length;
-    //     results = new Array(len);
-    //     for (i=0; i<len; i++) {
-    //       item = items[i];
-    //       if (isPromise(item)) {
-    //         hasPromises = true;
-    //         results[i] = item.then(func);
-    //       } else {
-    //         results[i] = func(item);
-    //       }
-    //     }
-    //     return hasPromises ? Promise.all(results) : results;
-    //   });
-    // }
-
-    return({
-      whenever: whenever,
-      map: map,
-      applyWhenever: applyAgnostic,
-      buildKozu: buildKozu
-    });
+    // ...
   }
 
-
-  root.kozu = buildKozu();
+  // var simple = {
+  //   apply: function() {},
+  //   call: function() {},
+  //   map: function() {},
+  //   filter: function() {},
+  //   compose: function() {},
+  // }
+  // var kozu = merge(simple, {
+  //   simple: simple,
+  //   agnostic: mapValues(simple, agnostic),
+  //   deepAgnostic: mapValues(simple, deepAgnostic),
+  //   forced: mapValues(simple, forced)
+  // });
+  // var myLib = mapValues(kozu.simple, myWrapper);
+  // var myComplexLib = mapValues(kozu.simple, compose(middleware1, middleware2));
+  root.kozu = kozu;
 })(this);
