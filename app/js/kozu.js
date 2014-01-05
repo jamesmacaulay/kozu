0ef1b98ab25dd89f3ba63bc7f2c474e3877c181f
James MacAulay <james@shopify.com>
2014-01-05 11:58:18 -0500

    WIP last night

--- app/js/kozu.js
+++ app/js/kozu.js
@@ -1,25 +1,128 @@
 (function(root){
-  var slice = Array.prototype.slice;
+  var protoSlice = Array.prototype.slice;
 
-  function isFunction(x) {
-    return typeof x === 'function';
-  }
+  function buildKozu(opts) {
+    var kozu = {};
+    // ===
+    // using underscore temporarily to sketch:
+    var extend = kozu.extend = _.extend;
+    var some = kozu.some = _.some;
+    var map = kozu.map = _.map;
+    var filter = kozu.filter = _.filter;
+    var reduce = kozu.reduce = _.reduce;
 
-  // "has a .then method"
-  function isThenable(x) {
-    return x != null && isFunction(x.then);
-  }
+    function merge() {
+      return _.extend({}, arguments);
+    }
+    kozu.merge = merge;
+    // ===
 
-  // build a promise
-  function promise(resolver) {
-    return new Promise(resolver);
-  }
+    function identity(x) {
+      return x;
+    }
+    kozu.identity = identity;
 
-  function buildKozu(deps) {
-    // default isPromise
-    var isPromise = (deps && deps.isPromise) || isThenable;
+    function args() {
+      return arguments;
+    }
+    kozu.args = args;
 
-    // ...
+    function functionalize(method) {
+      return function(ctx /*, args */) {
+        return method.apply(ctx, protoSlice.call(arguments, 1));
+      };
+    }
+    kozu.functionalize = functionalize;
+
+    var call = kozu.call = functionalize(Function.prototype.call);
+
+    var apply = kozu.apply = functionalize(Function.prototype.apply);
+
+    var slice = kozu.slice = functionalize(protoSlice);
+
+    // constructing(Array)(1,2,3) => [1,2,3]
+    // generated from CoffeeScript: (klass) -> new klass(arguments...)
+    function constructing(klass) {
+      return (function(func, args, Ctor) {
+        Ctor.prototype = func.prototype;
+        var child = new Ctor(), result = func.apply(child, args);
+        return Object(result) === result ? result : child;
+      })(klass, arguments, function(){});
+    }
+    kozu.constructing = constructing;
+
+    function isFunction(x) {
+      return typeof x === 'function';
+    }
+    kozu.isFunction = isFunction;
+
+    // "has a .then method"
+    function isThenable(x) {
+      return x != null && kozu.isFunction(x.then);
+    }
+    kozu.isThenable = isThenable;
+    var isPromise = kozu.isPromise = isThenable;
+
+    var makePromise = kozu.makePromise = function makePromise(resolver) {
+      return new Promise(resolver);
+    };
+
+    // TODO: try/catch and return rejecting promise on error
+    function whenever(value, func) {
+      return isPromise(value) ? value.then(func) : func(value);
+    }
+    kozu.whenever = whenever;
+
+    function all(items) {
+      return some(items, isPromise) ? Promise.all(items) : results;
+    }
+    kozu.all = all;
+
+    function cons(head, tail) {
+      return [head].concat(tail);
+    }
+
+    // function agnosticApply(func, ctx, args) {
+    //   return whenever(all([func, ctx].concat(args)), function(values) {
+    //     var func = values[0], ctx = values[1], 
+    //   });
+    // }
+
+    function agnostic(func) {
+      return (function() {
+        return whenever(all(cons(this, arguments)), function(ctxAndArgs) {
+          func.apply(ctxAndArgs[0], ctxAndArgs[1]);
+        });
+      });
+    }
+    kozu.agnostic = agnostic;
+
+    var agnosticApply = agnostic(apply);
+
+    // what would a generalized deep* function look like?
+    function deepAgnostic(func) {
+      if (isFunction(func)) {
+        return (function() {
+          // this isn't quite right:
+          // return agnosticApply(func, this, map(arguments, deepAgnostic));
+        });
+      } else {
+        return func;
+      }
+    }
+    kozu.deepAgnostic = deepAgnostic;
+
+    // 
+    // function infect(func, mod) {};
+
+    // override object properties
+    extend(kozu, opts);
+
+    // override just a few locals
+    isPromise = kozu.isPromise;
+    makePromise = kozu.makePromise;
+
+    return kozu;
   }
 
   // var simple = {
@@ -37,5 +140,5 @@
   // });
   // var myLib = mapValues(kozu.simple, myWrapper);
   // var myComplexLib = mapValues(kozu.simple, compose(middleware1, middleware2));
-  root.kozu = kozu;
+  root.kozu = buildKozu();
 })(this);
--- spec/kozu_spec.coffee
+++ spec/kozu_spec.coffee
@@ -1,4 +1,4 @@
-describe 'kozu', ->
+describe "kozu", ->
   async = new AsyncSpec(this)
   promises = (f) ->
     throw TypeError unless typeof f is 'function'
@@ -17,74 +17,7 @@ describe 'kozu', ->
   args = -> arguments
   double = _.partial(_.mul, 2)
 
-  describe ".whenever(value, func)", ->
-    it "returns func called with value immediately if value is not a promise", ->
-      picked = kozu.whenever({a: 1, b: 2}, picks('a'))
-      expect(picked).toEqual({a: 1})
-    async.it "returns a promise of the function application if value is a promise", (done) ->
-      picked = kozu.whenever(Promise.cast({a: 1, b: 2}), picks('a'))
-      picked.then (value) ->
-        expect(value).toEqual({a: 1})
-        done()
-    async.it "returns a promise when func is a promise", (done) ->
-      doubled = kozu.whenever(2, Promise.cast(double))
-      doubled.then (value) ->
-        expect(value).toBe(4)
-        done()
-
-  describe ".map(items, func)", ->
-    it "acts like regular map with an array of non-promises", ->
-      expect(kozu.map([1,2,3,4], double)).toEqual([2,4,6,8])
-    async.it "returns a promise of an array if items contains any promises", (done) ->
-      doubled = kozu.map([1,Promise.cast(2),3,Promise.cast(4)], double)
-      doubled.then (items) ->
-        expect(items).toEqual([2,4,6,8])
-        done()
-    async.it "returns a promise when func is a promise", (done) ->
-      doubled = kozu.map([1,2,3,4], Promise.cast(double))
-      doubled.then (items) ->
-        expect(items).toEqual([2,4,6,8])
-        done()
-
-  describe ".applyWhenever(func, ctx, args)", ->
-    it "acts like `apply` when working with non-promises", ->
-      picked = kozu.applyWhenever([].slice, [0,1,2], [1,2])
-      expect(picked).toEqual([1])
-    it "returns a promise if any of the arguments are promises", ->
-      # waiting for kozu.all
-
-  describe 'some other things', ->
-    whenever = (value, func) ->
-      if kozu.dependencies.isPromise(value)
-        value.then(func)
-      else
-        func(value)
-
-    doWhenever = (func) ->
-      (value) -> whenever(value, func)
-
-    reduce = (func, items) ->
-      result = items[0]
-      for item in Array.prototype.slice.call(items, 1)
-        result = func(result, item)
-      result
-
-    compose2 = (f, g) ->
-      (x) ->
-        f.call(this, g.apply(this, arguments))
-
-    compose = () ->
-      reduce(compose2, arguments)
-
-    identity = (x) -> x
-
-
-    composeWhenever = () ->
-      reduce(compose2, map(doWhenever, arguments))
-
-    it 'does something', ->
-      appending = (suffix) ->
-        (x) -> "#{x}#{suffix}"
-
-      f = compose(appending(4), appending(3), appending(2), appending(1))
-      expect(f("foo")).toBe("foo1234")
+  describe ".call", ->
+    it "acts like Function.prototype.call", ->
+      sliced = kozu.call(Array.prototype.slice, [1,2,3], 1, 2)
+      expect(sliced).toEqual([2])
