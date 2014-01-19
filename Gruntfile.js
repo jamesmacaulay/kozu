d55ba28bba36a3ae44e28e73b5dc68adf04ba8f0
James MacAulay <james@shopify.com>
2014-01-19 15:40:07 -0500

    blank slate: grunt + browserify + testem + mocha + chai-as-promised + coffee

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,74 @@
-/*global module:false*/
+'use strict';
+
 module.exports = function(grunt) {
-  require(process.env['LINEMAN_MAIN']).config.grunt.run(grunt);
+
+  // Project configuration.
+  grunt.initConfig({
+    coffee: {
+      test: {
+        expand: true,
+        cwd: 'test',
+        src: ['**/*.coffee'],
+        dest: 'generated/coffee/test',
+        ext: '.js'
+      }
+    },
+    browserify: {
+      lib: {
+        src: ['lib/kozu.js'],
+        dest: 'generated/browserify/lib/kozu.js',
+        options: {
+          standalone: 'kozu'
+        }
+      },
+      test: {
+        src: ['generated/coffee/test/common/kozu_test.js'],
+        dest: 'generated/browserify/test/common/kozu_test.js'
+      },
+    },
+    testem: {
+      main: {
+        src: ["test/browser/index.html"],
+        dest: 'tests.tap'
+      }
+    },
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      lib: {
+        src: ['lib/**/*.js']
+      },
+      test: {
+        src: ['test/common/**/*.js', 'test/node/**/*.js']
+      },
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      lib: {
+        files: '<%= jshint.lib.src %>',
+        tasks: ['jshint:lib', 'browserify:lib']
+      },
+      test: {
+        files: ['test/**/*', 'lib/**/*'],
+        tasks: ['jshint:test', 'coffee:test', 'browserify:test']
+      },
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-testem');
+
+  // Default task.
+  grunt.registerTask('default', ['jshint', 'coffee', 'browserify']);
+
 };
